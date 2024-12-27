9a340bbaf7f9f069a5430362001660a81ad8eca0
Prompt X <stanislav.semenov.2016@mail.ru>
2024-12-26 16:41:46 -0800

    Add map data for (Sandy Map Data) - Sheriff+Hospital+Airfield+Firestation

--- cfx_prompt_sandy_mapdata/sv_MapDataHandler.lua
+++ cfx_prompt_sandy_mapdata/sv_MapDataHandler.lua
@@ -4,9 +4,9 @@ local maps = {
     "prompt_sandy_airfield",
     "cfx_prompt_sandy_shores_fire_department"
 }
-
+ 
 local events = {}
-
+ 
 CreateThread(function()
     local exists = false 
     TriggerEvent("lyn-mapdata:exists", function(exists)
@@ -14,16 +14,16 @@ CreateThread(function()
             exists = true
         end
     end)
-
+ 
     if exists == true then 
         print("^6[Prompt]^1 Map data already exists. There must be only one mapdata installed!^0")
     end
 end)
-
+ 
 RegisterNetEvent("lyn-mapdata:exists", function(cb)
     cb(true)
 end)
-
+ 
 for i = 1, #maps do
     local eventName = maps[i] .. ":mapDataExists"
     if Debug == true then 
@@ -34,5 +34,5 @@ for i = 1, #maps do
     end)
     table.insert(events, event)
 end
-
+ 
 -- 
